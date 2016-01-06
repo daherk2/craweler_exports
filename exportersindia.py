eb73dda32eb0a3809996fd7c708c28263ff51aa9
Fabio RDB <fabio.rosindo.daher.de.barros@gmail.com>
2016-01-06 13:10:15 -0200

    update to filter content

--- exportersindia.py
+++ exportersindia.py
@@ -23,5 +23,34 @@ urls_interesteds = []
 for i in urls:
     if mainurl in i:
         urls_interesteds.append(i)
-        print i
-        
+        #print i
+
+telephone_list = open('telephone.txt','w')
+name_list = open('name.txt','w')
+address_list = open('address.txt','w')
+email_list = open('email.txt','w')
+#for j in urls_interesteds:
+print urls_interesteds[0]
+page = lib2.urlopen(urls_interesteds[0])
+content = page.readlines()
+telephone_tag = "icon-phone-alt dif list_dn"
+name_tag = "ml0px grid_dn vam si w16px icon-vtrust"
+address_tag = "span itemprop"
+email_tag = "hig grid_dn"
+print 'verificando tags'
+for i in content:
+    if telephone_tag in i:
+        telephone_list.write(i)
+    if name_tag in i:
+        name_list.write(i)
+    if address_tag in i:
+        address_list.write(i)
+    if email_tag in i:
+        email_list.write(i)    
+
+print 'tags verificadas'
+
+telephone_list.close()
+name_list.close()
+address_list.close()
+email_list.close()
